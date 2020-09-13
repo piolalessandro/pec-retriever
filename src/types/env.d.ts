675acaeda40bbd6bd1ab7d222c40396659c45d75
alesauro <piolalessandro@gmail.com>
2020-09-13 22:06:14 +0200

    changed database password env variable with full connection string

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
-    DATABASE_PASSWORD: string
+    DATABASE_CONNECTION_STRING: string
     SERVER_PORT: string
     CAPTCHA_API_KEY: string
     CORS_ORIGIN: string
